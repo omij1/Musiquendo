29911cae83f41c60dab462b7e3effcf3a0cf4bc2
Omar Marcos <omarcosjulian@gmail.com>
2018-04-16 21:58:35 +0200

    Añadidos iconos de búsqueda y filtro en la toolbar principal

--- app/src/main/java/com/example/mimo/musiquendo/MainActivity.java
+++ app/src/main/java/com/example/mimo/musiquendo/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.mimo.musiquendo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,17 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_albumes:
 
-                    return true;
-                case R.id.navigation_dashboard:
-
-                    return true;
-                case R.id.navigation_notifications:
-
-                    return true;
-            }
             return false;
         }
     };
@@ -46,14 +40,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        /*TabLayout tabs = findViewById(R.id.tabs);
+        ViewPager pager = findViewById(R.id.pager);
+        pager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
+        pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabs));
+        tabs.setupWithViewPager(pager);*/
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_items, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -63,6 +61,12 @@ public class MainActivity extends AppCompatActivity {
             case android.R.id.home:
                 drawer.openDrawer(GravityCompat.START);
             return true;
+            case R.id.search:
+                Toast.makeText(this, "hola", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.filter:
+                Toast.makeText(this, "hola", Toast.LENGTH_SHORT).show();
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
